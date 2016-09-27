69b7f7363ea632c4ed587b12e48e4d4be0b26f55
motemen <motemen@gmail.com>
2016-09-27 22:01:42 +0900

    accept *sql.DB or *sql.Tx in shortcut methods

--- db.go
+++ db.go
@@ -6,17 +6,23 @@ import (
 	"database/sql"
 )
 
-func (s SQL) Exec(db *sql.DB) (sql.Result, error) {
+type dbQueryer interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+func (s SQL) Exec(db dbQueryer) (sql.Result, error) {
 	query, args := s.BuildSQL()
 	return db.Exec(query, args...)
 }
 
-func (s SQL) Query(db *sql.DB) (*sql.Rows, error) {
+func (s SQL) Query(db dbQueryer) (*sql.Rows, error) {
 	query, args := s.BuildSQL()
 	return db.Query(query, args...)
 }
 
-func (s SQL) QueryRow(db *sql.DB) *sql.Row {
+func (s SQL) QueryRow(db dbQueryer) *sql.Row {
 	query, args := s.BuildSQL()
 	return db.QueryRow(query, args...)
 }
